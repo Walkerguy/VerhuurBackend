1edf1c7de95c58910334cbd4fe3e6c61f776be68
Bram <shep.ukn@gmail.com>
2019-01-16 14:24:07 +0100

    Config file update

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,26 +1,17 @@
 // Environment variabelen.
-
-var secret = 'ibahesh_ah_sahbe';
-
-var env =
+var env = 
 {
-    webPort: process.env.PORT || 3000,
-    dbHost: process.env.DB_HOST || 'localhost',
-    dbPort: process.env.DB_PORT || '',
-    dbUser: process.env.DB_USER || '',
-    dbPassword: process.env.DB_PASSWORD || '',
-    dbDatabase: process.env.DB_DATABASE || 'Tools'
+    webPort:        process.env.PORT || 3000,
+    dbHost:         process.env.DB_HOST || 'localhost',
+    dbPort:         process.env.DB_PORT || '',
+    dbUser:         process.env.DB_USER || '',
+    dbPassword:     process.env.DB_PASSWORD || '',
+    dbDatabase:     process.env.DB_DATABASE || 'Tools'
 }
 
-var dburl = 'mongodb://localhost/' + env.dbDatabase
-
-   // 'mongodb://localhost/' + env.dbDatabase, //"mongodb://" + env.dbUser + ":" + env.dbPassword + "@" + env.dbHost + ":" + env.dbPort + "/" + env.dbDatabase,
-   //dburl: "mongodb://" + env.dbUser + ":" + env.dbPassword + "@ds029585.mlab.com:29585/mongodb_project"
-
-
-
-module.exports = {
+module.exports = 
+{
     env: env,
-    dburl: dburl,
+    dburl: "mongodb://" + env.dbUser + ":" + env.dbPassword + "@" + env.dbHost + ":" + env.dbPort + "/" + env.dbDatabase,
     secret: secret
 };
